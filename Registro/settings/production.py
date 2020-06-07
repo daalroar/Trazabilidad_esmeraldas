eab0db6031440c86b613126abf34a448c737892b
Alejandro Romero <daalroar@gmail.com>
2020-06-07 16:16:38 -0500

    credenciales postgres comprobadas otra vez

--- Registro/settings/production.py
+++ Registro/settings/production.py
@@ -1,11 +1,9 @@
 from Registro.settings.base import*
 
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://trazabilidad-esmeraldas.herokuapp.com/']
-
+ALLOWED_HOSTS = ['trazabilidad-esmeraldas.herokuapp.com/']
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
