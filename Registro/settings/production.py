c267a922e52feb3c850759a4c758e26d4041d36f
Alejandro Romero <daalroar@gmail.com>
2020-06-07 17:41:46 -0500

    debug en produccion a false

--- Registro/settings/production.py
+++ Registro/settings/production.py
@@ -1,7 +1,7 @@
 from Registro.settings.base import*
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['trazabilidad-esmeraldas.herokuapp.com']
 
